33443280ac4416e1f05fc8e5ab107e7d860ff37c
Christopher Carrillo <carrillocrespo17@gmail.com>
2026-02-02 19:20:54 +0100

    Primer intento de creacion de tablas

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { sequelize } = require('./models');
+const { sequelize } = require('./src/models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
